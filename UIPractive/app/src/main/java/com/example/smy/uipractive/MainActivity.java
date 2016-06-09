b6369846c53ec0847be307bf522fc8661662f3e1
sun.mengying <sun.mengying@xiaoyi.com>
2016-06-09 12:15:46 +0800

    ui practice completed

--- UIPractive/app/src/main/java/com/example/smy/uipractive/MainActivity.java
+++ UIPractive/app/src/main/java/com/example/smy/uipractive/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.smy.uipractive;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private ListView msgListView;
+    private EditText inputText;
+    private Button send;
+    private MsgAdapter adapter;
+    private List<Msg> msgList = new ArrayList<Msg>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        initMsg();
+        adapter = new MsgAdapter(MainActivity.this, R.layout.msg_item, msgList);
+        inputText = (EditText) findViewById(R.id.input_text);
+        send = (Button) findViewById(R.id.send);
+        msgListView = (ListView) findViewById(R.id.msg_list_view);
+        msgListView.setAdapter(adapter);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = inputText.getText().toString();
+                if(!"".equals(content))
+                {
+                    Msg msg = new Msg(content, Msg.TYPE_SEND);
+                    msgList.add(msg);
+                    adapter.notifyDataSetChanged();
+                    msgListView.setSelection(msgList.size());
+                    inputText.setText("");
+                }
+            }
+        });
+
+    }
+
+    private void initMsg()
+    {
+        Msg msg1 = new Msg("Hello James.", Msg.TYPE_RECEIVED);
+        msgList.add(msg1);
+        Msg msg2 = new Msg("Hello.Who's there?", Msg.TYPE_SEND);
+        msgList.add(msg2);
+        Msg msg3 = new Msg("This is Lebron.Nice meeting you.", Msg.TYPE_RECEIVED);
+        msgList.add(msg3);
     }
 }
