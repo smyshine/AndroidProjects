16001f12ae4a8154bd7ec694d0e00a641f160eae
smyshine <1542819570@qq.com>
2017-11-22 17:28:34 +0800

    add screen record

--- ScreenCapture/app/src/main/java/smy/com/screencapture/MainActivity.kt
+++ ScreenCapture/app/src/main/java/smy/com/screencapture/MainActivity.kt
@@ -1,19 +1,82 @@
 package smy.com.screencapture
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.media.projection.MediaProjectionManager
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        screenShot.setOnClickListener(View.OnClickListener {
-            startActivity(Intent(this, ScreenShotActivity::class.java))
-        })
+        screenShot.setOnClickListener(this)
+        startRecord.setOnClickListener(this)
+        stopRecord.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0!!.id){
+            R.id.screenShot -> startShot()
+            R.id.startRecord -> {
+                isRecord = true
+                startShot()
+            }
+            R.id.stopRecord -> stopShot()
+        }
+    }
+
+    private var isRecord : Boolean = false
+    private var mShotter : ScreenShotter? = null
+
+    private fun startShot(){
+        requestScreenShot()
+    }
+
+    private fun stopShot(){
+        if (isRecord){
+            mShotter!!.stopRecord()
+            isRecord = false
+        }
+    }
+
+    private fun requestScreenShot(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            startActivityForResult(
+                    (getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager).createScreenCaptureIntent(),
+                    ScreenShotActivity.REQUEST_MEDIA_PROJECTION)
+        } else {
+            Toast.makeText(this, "Too low to shot", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode){
+            ScreenShotActivity.REQUEST_MEDIA_PROJECTION -> {
+                if (resultCode == Activity.RESULT_OK && data != null){
+                    mShotter = ScreenShotter(this, data,
+                            ScreenUtil.getScreenRealWidth(this), ScreenUtil.getScreenRealHeight(this))
+                    if (isRecord){
+                        mShotter!!.startScreenRecord { path -> handleScreenShotResult(path) }
+                        moveTaskToBack(true)
+                    } else {
+                        mShotter!!.startScreenShot { path -> handleScreenShotResult(path) }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleScreenShotResult(path: String){
+        runOnUiThread(Runnable { tvResult.text = path })
     }
 }
--- ScreenCapture/app/src/main/java/smy/com/screencapture/ScreenShotter.java
+++ ScreenCapture/app/src/main/java/smy/com/screencapture/ScreenShotter.java
@@ -11,6 +11,10 @@ import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
 import android.media.Image;
 import android.media.ImageReader;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.os.AsyncTask;
@@ -18,12 +22,16 @@ import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.view.Surface;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.ref.SoftReference;
 import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static android.content.Context.MEDIA_PROJECTION_SERVICE;
 
 /**
  * Created by SMY on 2017/11/22.
@@ -42,17 +50,23 @@ public class ScreenShotter {
     private Handler handler = new Handler();
 
     public ScreenShotter(Context context, Intent data, int mScreenWidth, int mScreenHeight) {
+        this(context, data, mScreenWidth, mScreenHeight, false);
+    }
+
+    public ScreenShotter(Context context, Intent data, int mScreenWidth, int mScreenHeight, boolean record) {
         this.mRefContext = new SoftReference<Context>(context);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
             this.mScreenWidth = mScreenWidth;
             this.mScreenHeight = mScreenHeight;
             mMediaProjection = getMediaProjectionManager().getMediaProjection(Activity.RESULT_OK, data);
-            mImageReader = ImageReader.newInstance(mScreenWidth, mScreenHeight, PixelFormat.RGBA_8888, 1);
+            if (!record){
+                mImageReader = ImageReader.newInstance(mScreenWidth, mScreenHeight, PixelFormat.RGBA_8888, 1);
+            }
         }
     }
 
     private MediaProjectionManager getMediaProjectionManager(){
-        return (MediaProjectionManager) getContext().getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        return (MediaProjectionManager) getContext().getSystemService(MEDIA_PROJECTION_SERVICE);
     }
 
     private Context getContext(){
@@ -165,6 +179,126 @@ public class ScreenShotter {
 
     public interface OnShotListener{
         void onFinish(String path);
+    }
+
+
+    private MediaCodec mediaCodec;
+    private Surface surface;
+    private MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+    private MediaMuxer mediaMuxer;
+    private AtomicBoolean mQuit = new AtomicBoolean(false);
+    private int videoTrackIndex = -1;
+    private boolean muxerStarted = false;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void startScreenRecord(OnShotListener listener){
+        mShotListener = listener;
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    prepareRecord();
+                    mVirtualDisplay = mMediaProjection.createVirtualDisplay("ScreenShout",
+                            mScreenWidth,
+                            mScreenHeight,
+                            Resources.getSystem().getDisplayMetrics().densityDpi,
+                            DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC,
+                            surface,
+                            null,
+                            null);
+
+                    recordVirtualDisplay();
+                } finally {
+                    release();
+                }
+            }
+        }.start();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void release(){
+        if (mediaCodec != null){
+            mediaCodec.stop();
+            mediaCodec.release();
+            mediaCodec = null;
+        }
+        if (mVirtualDisplay != null){
+            mVirtualDisplay.release();
+        }
+        if (mMediaProjection != null){
+            mMediaProjection.stop();
+        }
+        if (mediaMuxer != null){
+            mediaMuxer.release();
+            mediaMuxer = null;
+        }
+        if (mShotListener != null){
+            mShotListener.onFinish(mLocalUrl);
+        }
+    }
+
+    public void stopRecord(){
+        mQuit.set(true);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void prepareRecord(){
+        MediaFormat format = MediaFormat.createVideoFormat("video/avc", mScreenWidth, mScreenHeight);
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        format.setInteger(MediaFormat.KEY_BIT_RATE, 6000000);
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 10);
+        try {
+            mediaCodec = MediaCodec.createEncoderByType("video/avc");
+            mediaCodec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+            surface = mediaCodec.createInputSurface();
+            mediaCodec.start();
+            if (TextUtils.isEmpty(mLocalUrl)){
+                File dir = new File(APP_DIR);
+                if (!dir.exists()){
+                    dir.mkdirs();
+                }
+                mLocalUrl = APP_DIR + System.currentTimeMillis() + ".mp4";
+            }
+            mediaMuxer = new MediaMuxer(mLocalUrl, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void recordVirtualDisplay(){
+        while (!mQuit.get()){
+            int index = mediaCodec.dequeueOutputBuffer(bufferInfo, 10000);
+            if (index == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED){
+                resetOutputFormat();
+            } else if (index >= 0){
+                encodeToVideoTrack(index);
+                mediaCodec.releaseOutputBuffer(index, false);
+            }
+        }
+    }
+
+    private void resetOutputFormat(){
+        MediaFormat newFormat = mediaCodec.getOutputFormat();
+        videoTrackIndex = mediaMuxer.addTrack(newFormat);
+        mediaMuxer.start();
+        muxerStarted = true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void encodeToVideoTrack(int index){
+        ByteBuffer encodeData = mediaCodec.getOutputBuffer(index);
+        if ((bufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0){
+            bufferInfo.size = 0;
+        }
+        if (bufferInfo.size == 0){
+            encodeData = null;
+        }
+        if (encodeData != null){
+            encodeData.position(bufferInfo.offset);
+            encodeData.limit(bufferInfo.offset + bufferInfo.size);
+            mediaMuxer.writeSampleData(videoTrackIndex, encodeData, bufferInfo);
+        }
     }
 
 }
