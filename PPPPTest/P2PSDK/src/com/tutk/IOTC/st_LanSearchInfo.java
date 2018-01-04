d29000bc90b1ca2c861652b3026cf5140c8c1273
smyshine <1542819570@qq.com>
2018-01-04 13:24:22 +0800

    delete useless files

--- PPPPTest/P2PSDK/src/com/tutk/IOTC/st_LanSearchInfo.java
+++
@@ -1,7 +0,0 @@
-package com.tutk.IOTC;
-
-public class st_LanSearchInfo {
-	public byte[] UID= null;
-	public byte[] IP = null;
-	public int port=0;
-}
--- PPPPTest/P2PSDK/src/com/tutk/IOTC/st_LanSearchInfo2.java
+++
@@ -1,8 +0,0 @@
-package com.tutk.IOTC;
-
-public class st_LanSearchInfo2 {
-	public byte[] UID= null;
-	public byte[] IP = null;
-	public int port=0;
-	public byte[] DeviceName= null;
-}
